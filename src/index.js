cce1a2592416953bc19036e3e54d315d93b89eaa
João Tavares <joaotavares@MacBook-Pro-de-Joao.local>
2023-08-26 21:10:49 -0300

    added commentaries with the default value of each key in the list

--- src/index.js
+++ src/index.js
@@ -12,18 +12,9 @@ class Lantern {
   }
 
   parse() {
-    this.phrase = this.phrase.toLowerCase()
-      .normalize("NFD")      
-      .replaceAll(',', '')
-      .replaceAll('.', '')
-      .replaceAll('!', '')
-      .replaceAll('?', '')
-      .replaceAll(`'`, '')
-      .replaceAll(`"`, '')
-      .replace(/[\u0300-\u036f]/g, "")
-      .split(' ');
+    this.phrase = new PepperSpray(this.phrase);
 
-    
+    this.phrase
   }
 
   translate() {
@@ -36,103 +27,103 @@ class Lantern {
   }
 
   hand() { //Shou
-
+    // default: (old mc donald had a fart, fart fart fart fart fart)
   }
 
   field() { //Tian
-
+    // default: eye
   }
 
   water() { //Shui
-
+    // default: too
   }
 
   mouth() { //Kou
-
+    // default: vinegar
   }
 
   twenty() { //Nian
-
+    // default: hand
   }
 
   predict() { //Bo
-
+    // default: in
   }
 
   mountain() { //Shan
-
+    // default: zo
   }
 
   daggerAxe() { //Ge
-
+    // default: degree
   }
 
   people() { //Ren
-
+    // default: eye
   }
 
   heart() { //Xin
-
+    // default: dimple
   }
 
   day() { //Ri
-
+    // default: picture
   }
 
   corpse() { //Shi
-
+    // default: in
   }
 
   wood() { //Mu
-
+    // default: oh
   }
 
   fire() { //Huo
-
+    // default: hey
   }
 
   earth() { //Tu
-
+    // default: ho
   }
 
   bamboo() { //Zhu
-
+    // default: reactor
   }
 
   ten() { //Shi
-
+    // default: dimple
   }
 
   big() { //Da
-
+    // default: ma
   }
 
   middle() { //Zhong
-
+    // default: vinegar
   }
 
   disaster() { //Nan
-
+    // default: ru
   }
 
   gold() { //Jin
-
+    // default: ingredient
   }
 
   female() { //Nu
-
+    // default: hot water (👺)
   }
 
   moon() { //Yue
-
+    // default: fruit
   }
 
   bow() { //Gong
-
+    // default: nothing
   }
 
   one() { //Yi
-
+    // default: hand
   }
 }
 
